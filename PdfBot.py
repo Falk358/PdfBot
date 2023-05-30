4d1d46bf7ffa3efab3e7b81520267f8526a81b4b
Falk358 <maxdanelli@gmail.com>
2023-05-30 20:32:51 +0200

    added error handling for pdf loading and working on reading pdf from given path

--- PdfBot.py
+++ PdfBot.py
@@ -5,13 +5,21 @@ from langchain.vectorstores import Pinecone
 from langchain.memory import ChatMessageHistory
 from langchain.chains import AnalyzeDocumentChain, ChatVectorDBChain, LLMChain, SequentialChain
 from langchain.document_loaders import PyPDFLoader
+from os import path
 #from langchain.text_splitter import 
 
-class PdfRepl():
-    def __init__(self, dir: str, document: str) -> None:
-        self.docpath = "".join([dir,"/",document])
-        self.load_pdf(self.docpath)
-    
+class PdfRepresentation():
+    def __init__(self, dir: str, document_filepath: str) -> None:
+        if self.__checkPath__(document_filepath):
+            self.docpath = "".join([dir,"/", document_filepath])
+            self.load_pdf(self.docpath)
+        else:
+            print(f"file {document_filepath} doesn't exist!")
+            exit(-1)
+   
+    def __checkPath__(self, filepath: str) -> bool:
+        return path.exists(filepath)
+        
     def load_pdf(self, path: str)->list:
         loader = PyPDFLoader(path)
         data = loader.load_and_split()
@@ -32,7 +40,9 @@ def getEmbeddings():
 
 def main()-> None:
     my_env = sysenv.load(".env")
-    my_pdf = PdfRepl("pdf_library","core_rulebook_wrath_glory.pdf")
+    filepath = input("Please enter path to PDF you want to chat to:")
+    path_to_file, filename = filepath.split("/")
+    my_pdf = PdfRepresentation(path_to_file, filename)
     pages = my_pdf.getPages()
     print(pages[0])
 
